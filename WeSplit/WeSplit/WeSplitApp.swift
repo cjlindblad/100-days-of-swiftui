d45c23f6e4353dd30fdbd37813023e8929449578
Carl-Johan Lindblad <carljohan.lindblad@gmail.com>
2024-01-06 16:43:26 +0100

    Added WeSplit Xcode project.

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Carl-Johan Lindblad on 2024-01-06.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
