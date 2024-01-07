a54fdc9a94bfcc9b43747ae94937ed7e8fd72987
Carl-Johan Lindblad <carljohan.lindblad@gmail.com>
2024-01-07 20:04:17 +0100

    Added UnitConverter project.

---
+++ UnitConverter/UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Carl-Johan Lindblad on 2024-01-07.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
