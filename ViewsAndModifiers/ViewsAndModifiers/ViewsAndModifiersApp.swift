481ec7512ca0b50d004dda45cb72369a5e6fa71a
Carl-Johan Lindblad <carl.lindblad@sr.se>
2024-01-08 14:48:10 +0100

    Added ViewsAndModifiers project.

---
+++ ViewsAndModifiers/ViewsAndModifiers/ViewsAndModifiersApp.swift
@@ -0,0 +1,17 @@
+//
+//  ViewsAndModifiersApp.swift
+//  ViewsAndModifiers
+//
+//  Created by Carl-Johan Lindblad on 2024-01-08.
+//
+
+import SwiftUI
+
+@main
+struct ViewsAndModifiersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
