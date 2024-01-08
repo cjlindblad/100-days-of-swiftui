527e21a543204d484b3c833d94fbcfc43ecab3a9
Carl-Johan Lindblad <carl.lindblad@sr.se>
2024-01-08 12:31:19 +0100

    Day 20.

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,14 +8,24 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showingAlert = false
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Button("Show alert") {
+                showingAlert = true
+            }
+            .alert("Important message", isPresented: $showingAlert) {
+                Button("Delete", role: .destructive) {}
+                Button("Cancel", role: .cancel) {}
+            } message: {
+                Text("Please read this.")
+            }
         }
-        .padding()
+    }
+    
+    func executeDelete() {
+        print("Now deleting..")
     }
 }
 
