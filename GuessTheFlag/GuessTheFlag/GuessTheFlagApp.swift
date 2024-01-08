fd432aef98ca7305114cea642b7c55067e15efbd
Carl-Johan Lindblad <carl.lindblad@sr.se>
2024-01-08 11:07:28 +0100

    Added GuessTheFlag project.

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Carl-Johan Lindblad on 2024-01-08.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
