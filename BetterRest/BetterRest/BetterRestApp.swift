b0e591f3f93014c76632f77689d99f3c933aae3e
Carl-Johan Lindblad <carl.lindblad@sr.se>
2024-01-09 13:16:37 +0100

    Day 26.

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Carl-Johan Lindblad on 2024-01-09.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
